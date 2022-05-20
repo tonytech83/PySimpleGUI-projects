d1b640bd1b6040d3432983c8ea21345c900bc924
Anton Petrov <a.petrov.83@gmail.com>
2022-05-20 15:58:31 +0300

    Add project for adding data to existing Excel file

---
+++ Add data to existing Excel file/excel_data_entry.py
@@ -0,0 +1,44 @@
+import PySimpleGUI as sg
+import pandas as pd
+
+# add some color to the window
+sg.theme("DarkTeal9")
+
+EXCEL_FILE = "excel_data_entry.xlsx"
+df = pd.read_excel(EXCEL_FILE)
+
+layout = [
+    [sg.Text("Please fill out the following fields:")],
+    [sg.Text("Date", size=(10, 1)), sg.InputText(key="DATE")],
+    [sg.Text("Exchange", size=(10, 1)), sg.Combo(["Kraken", "eToro", "Binance"], key="EXCHANGE")],
+    [sg.Text("Asset", size=(10, 1)), sg.InputText(key="ASSET")],
+    #    [sg.Text("Open Price", size=(10, 1)), sg.InputText(key="OPENPRICE")],
+    [sg.Text("Units", size=(10, 1)), sg.InputText(key="UNITS")],
+    [sg.Text("Invest", size=(10, 1)), sg.InputText(key="INVEST")],
+    [sg.Text("Type", size=(10, 1)), sg.Combo(["Reward", "Buy", "Sell"], key="TYPE")],
+    [sg.Submit(), sg.Button("Clear"), sg.Exit()]
+]
+
+window = sg.Window("Simple data entry form", layout)
+
+
+def clear_input():
+    for key in values:
+        window[key]("")
+    return None
+
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == "Exit":
+        break
+
+    if event == "Clear":
+        clear_input()
+
+    if event == "Submit":
+        df = df.append(values, ignore_index=True)
+        df.to_excel(EXCEL_FILE, index=False)
+        sg.popup("Data saved!")
+        clear_input()
+window.close()
